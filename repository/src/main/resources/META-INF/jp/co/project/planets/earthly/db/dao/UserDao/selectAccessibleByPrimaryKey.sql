1e9f9842b926e7bee12488f07b7f477d1f8be967
M.Tamura <ma.tamutamu@gmail.com>
2022-09-18 14:54:46 +0900

    github actions対応

---
+++ repository/src/main/resources/META-INF/jp/co/project/planets/earthly/db/dao/UserDao/selectAccessibleByPrimaryKey.sql
@@ -0,0 +1,9 @@
+SELECT /*%expand*/*
+FROM user
+WHERE id = /* id */'a'
+  AND is_deleted = 0
+  /*%if !hasViewAllCompany */
+  AND company_id = (SELECT user_01.company_id
+                    FROM user user_01
+                    WHERE user_01.id = /* executionUserId */'a')
+/*%end */

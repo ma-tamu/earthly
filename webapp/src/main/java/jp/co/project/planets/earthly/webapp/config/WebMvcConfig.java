91f44fc91e5f0d2ca2f418b9a6313fc9cec3a956
MSI\masat <ma.tamutamu@gmail.com>
2023-11-11 15:04:02 +0900

    ログフォーマット設定

--- webapp/src/main/java/jp/co/project/planets/earthly/webapp/config/WebMvcConfig.java
+++ webapp/src/main/java/jp/co/project/planets/earthly/webapp/config/WebMvcConfig.java
@@ -2,12 +2,14 @@ package jp.co.project.planets.earthly.webapp.config;
 
 import java.nio.charset.StandardCharsets;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import jp.co.project.planets.earthly.webapp.filter.RequestFilter;
 import jp.co.project.planets.earthly.webapp.view.dialect.CustomDialect;
 
 /**
@@ -37,5 +39,17 @@ public class WebMvcConfig implements WebMvcConfigurer {
         messageSource.setBasename("classpath:i18n/messages");
         messageSource.setDefaultEncoding(StandardCharsets.UTF_8.name());
         return messageSource;
+    }
+
+    /**
+     * generate filter registration bean
+     * 
+     * @return FilterRegistrationBean
+     */
+    @Bean
+    public FilterRegistrationBean<RequestFilter> requestFilterFilterRegistrationBean() {
+        final var filterRegistrationBean = new FilterRegistrationBean<>(new RequestFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
     }
 }
